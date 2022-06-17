76bce491c9cba4957509530f12394b6d8ea75976
Juan Carlos <64619934+Nightsnake763@users.noreply.github.com>
2022-06-17 05:24:49 -0500

    images

--- sources/js/index.js
+++ sources/js/index.js
@@ -1,2 +1,2 @@
 var random_number = Math.floor((Math.random() * (1 - 1 + 1)) + 1);
-document.body.style = 'background: url("https://raw.githubusercontent.com/Nightsnake763/extension/main/images/' + random_number.toString() + '.png"); background-position: center top; background-repeat: no-repeat;'
+document.body.style = 'background: url("https://raw.githubusercontent.com/Nightsnake763/extension/main/images/' + random_number.toString() + '.jpg"); background-position: center top; background-repeat: no-repeat;'
